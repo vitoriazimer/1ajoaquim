3d4a186c42901b96940d45598fc378818d86437c
vitoriazimer <162469120+vitoriazimer@users.noreply.github.com>
2024-09-26 12:04:31 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//Outer Banks,LIVRE
+// Minha vida com a familia Walter,12
+// É Assim que acaba,10
+//Divertidamente 2
+// o Bicho vai pegar 4,
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background(220);
+}
