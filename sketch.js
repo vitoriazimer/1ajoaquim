e015da6a3d4df2a635114fa9d170923a4a467a66
vitoriazimer <162469120+vitoriazimer@users.noreply.github.com>
2024-10-17 11:56:22 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,52 @@
-//Outer Banks,LIVRE
-// Minha vida com a familia Walter,12
-// É Assim que acaba,10
-//Divertidamente 2
-// o Bicho vai pegar 4,
+//// fantasia, aventura, drama
 
+// a viagem de chihiro, LIVRE, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+// as aventuras de pi, 10, drama, fantasia, aventura
+// É Assim que acaba, 10, drama
+// onde nasce os sonhos, 12, fantasia, aventura
+// Rainha das lagrimas, 12, drama
+// Star wars, 14, drama
+//let campoIdade;
+let campoFantasia;
 
 function setup() {
-  createCanvas(400, 400);
-  
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
 }
 
+
 function draw() {
-  background(220);
+    background("#0DD3EC96");
+    let idade = campoIdade.value();
+    let gostaDeFantasia = campoFantasia.checked();
+    let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
+
+    fill(color(76, 0, 115));
+    textAlign(CENTER, CENTER);
+    textSize(38);
+    text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeFantasia) {
+    if (idade >= 10) {
+        if (idade >= 14) {
+            return "É assim que acaba";
+        } else {
+            if (gostaDeFantasia) {
+                return "onde nasce os sonhos ";
+            } else {
+                return "Rainha das lagrimas";
+            }
+        }
+    } else {
+        if (gostaDeFantasia) {
+            return "A viagem de chihiro";
+        } else {
+            return "O feitiço do tempo";
+        }
+    }
 }
